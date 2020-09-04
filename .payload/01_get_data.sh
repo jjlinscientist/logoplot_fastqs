8295df217c3c1f349b364a6479057703aaee83c2
justin <justin.jnn.lin@gmail.com>
2020-09-03 21:19:03 -0700

    3

--- .payload/01_get_data.sh
+++
@@ -1,9 +0,0 @@
-mkdir data/chimerdb
-wget https://www.kobic.re.kr/chimerdb_mirror/downloads?name=ChimerDB4.0_sequence.txt.gz \
-  -O data/chimerdb/sequence.txt.gz
-wget https://www.kobic.re.kr/chimerdb_mirror/downloads?name=Recurrent_table.xlsx \
-  -O data/chimerdb/table.xlsx
-wget https://www.kobic.re.kr/chimerdb_mirror/downloads?name=ChimerSeqV41.sql \
-  -O data/chimerdb/sequence.sql
-
-gunzip data/chimerdb/sequence.txt.gz
